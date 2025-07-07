eaec1efa6186c82f97b81ae21c81a32a8bb933ab
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-07-07 15:59:15 -0300

    feat: refactor page layout to display activities and remove unused buttons

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,29 +2,43 @@ import { Button } from '@/components/button'
 import { ExerciseCard } from '@/components/exercise-card'
 import { styles } from '@/styles'
 import { useRouter } from 'expo-router'
+import { useEffect, useState } from 'react'
 
 import { ScrollView, Text, View } from 'react-native'
+import * as SQLite from 'expo-sqlite'
+import { getAtividades } from '@/lib/database/get-atividades'
+
+const db = SQLite.openDatabaseSync('gymday.db')
 
 const Page = () => {
   const router = useRouter()
+  const [atividades, setAtividades] = useState([])
 
+  useEffect(() => {
+    getAtividades(db).then((data) => {
+      setAtividades(data)
+    })
+  })
   return (
     <View style={styles.container}>
       <Text style={[styles.title, { marginTop: 16 }]}>GymDay</Text>
-      <View style={{ flexDirection: 'row', gap: 8, maxWidth: '100%', justifyContent: 'space-between' }}>
-        <Button
+      {atividades.length > 0 ? (
+        <ScrollView style={{ width: '100%' }}>
+          <ExerciseCard id="1" title="Supino Reto" />
+        </ScrollView>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Text style={{ textAlign: 'center', marginTop: 16 }}>
+            Nenhuma atividade cadastrada
+          </Text>
+        </View>
+      )}
+      <Button
         title="Adicionar atividade"
         onPress={() => router.navigate('/nova-atividade')}
       />
-        <Button
-          title="Gerenciar exercícios"
-          onPress={() => router.navigate('/exercicios')}
-          variant="secondary"
-        />
-      </View>
-      <ScrollView style={{ width: '100%' }}>
-          <ExerciseCard id="1" title="Supino Reto" />
-        </ScrollView>
     </View>
   )
 }

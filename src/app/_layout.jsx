c5c9d2f2149e452bf097942a2e1d3864f099f981
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-07-07 15:59:00 -0300

    feat: initialize SQLite database and create table on layout mount

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,6 +1,15 @@
+import { createTable } from '@/lib/database/create-table'
 import { Stack } from 'expo-router'
+import { useEffect } from 'react'
+import * as SQLite from 'expo-sqlite'
+
+const db = SQLite.openDatabaseSync('gymday.db')
 
 export default function Layout() {
+  useEffect(() => {
+    createTable(db)
+  }, [])
+
   return (
     <Stack
       screenOptions={{
